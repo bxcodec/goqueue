fc4b0d6ad767e8d3bb5819283e1c10a253e818d4
Iman Tumorang <iman@tumorang.com>
2024-06-16 13:06:47 +0300

    chore: add sleep time for integration time

--- internal/consumer/rabbitmq/blackbox_consumer_test.go
+++ internal/consumer/rabbitmq/blackbox_consumer_test.go
@@ -43,6 +43,8 @@ func TestSuiteRabbitMQConsumer(t *testing.T) {
 		t.Skip("Skip the Test Suite for RabbitMQ Consumer")
 	}
 
+	time.Sleep(5 * time.Second) // wait for the rabbitmq to be ready
+
 	rmqURL := os.Getenv("RABBITMQ_TEST_URL")
 	if rmqURL == "" {
 		rmqURL = "amqp://test:test@localhost:5672/test"

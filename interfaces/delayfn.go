218c5ba19f1172482b84f2f7893b0c8387835e0a
Iman Tumorang <iman@tumorang.com>
2024-08-11 18:37:46 +0300

    chore: cleanup

--- interfaces/delayfn.go
+++ interfaces/delayfn.go
@@ -1,21 +1,17 @@
 package interfaces
 
-import "fmt"
-
 type DelayFn func(retries int64) (delay int64)
 
 var (
 	// ExponentialBackoffDelayFn is a delay function that implements exponential backoff.
 	// It takes the number of retries as input and returns the delay in seconds.
 	ExponentialBackoffDelayFn DelayFn = func(retries int64) (delay int64) {
-		fmt.Println(">>>>> ExponentialBackoffDelayFn, retry: ", retries)
 		return 2 << (retries - 1)
 	}
 
 	// LinearDelayFn is a delay function that implements linear delay.
 	// It takes the number of retries as input and returns the delay in seconds.
 	LinearDelayFn DelayFn = func(retries int64) (delay int64) {
-		fmt.Println(">>>>> LinearDelayFn, retry: ", retries)
 		return retries
 	}
 
